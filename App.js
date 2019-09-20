fa340cd8915e0541371ffed0ae5c71e82fdd4880
Imran Sayed <sayedimrankashmiri@gmail.com>
2019-09-20 21:17:31 +0530

    Project Ready for Deployment

--- App.js
+++ App.js
@@ -140,7 +140,13 @@ document.querySelector("#book-form").addEventListener("submit", e => {
 //Events : Remove the Books
 
 document.querySelector("#book-list").addEventListener("click", e => {
+  // Remove book from UI
+
   UI.deleteBook(e.target);
+
+  // Remove Book from Storage
+
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Show Delete Message
 
